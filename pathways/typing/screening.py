00169e387bc6b2ce38bea2483d4ec5a294b3e15c
Yann Forget <yannforget@mailbox.org>
2024-10-10 02:28:53 +0200

    feat: add function to add choices for screening questions

--- pathways/typing/screening.py
+++ pathways/typing/screening.py
@@ -32,7 +32,7 @@ def add_screening_questions(
 
         row = {
             "type": question_type,
-            "name": question["question_name"],
+            "name": question["name"],
             "calculation": question.get("calculation"),
             "relevant": question.get("relevant"),
             "required": required,
@@ -62,3 +62,35 @@ def add_screening_questions(
 
     new_worksheet = begin + survey_worksheet + end
     return new_worksheet
+
+
+def add_screening_choices(
+    choices_worksheet: list[dict], screening_config: list[dict], choices_config: dict
+) -> list[dict]:
+    """Add screening choices to the choices worksheet.
+
+    Args:
+        choices_worksheet (list[dict]): choices worksheet
+        screening_config (list[dict]): screening configuration data with one dict per screening
+            question
+        choices_config (dict): choices configuration data with choice list name as key
+    Returns:
+        list[dict]: updated choices worksheet
+    """
+    for question in screening_config:
+        choice_list = question.get("choice_list")
+
+        if choice_list:
+            for choice in choices_config[choice_list]:
+                row = {
+                    "list_name": choice_list,
+                    "value": choice["name"],
+                }
+
+                for label_column, label in choice["label"].items():
+                    row[label_column] = label
+
+                if row not in choices_worksheet:
+                    choices_worksheet.append(row)
+
+    return choices_worksheet
