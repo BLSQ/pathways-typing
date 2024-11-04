0c92d3af43267763faf7cea1c358490af1b50e30
Yann Forget <yannforget@mailbox.org>
2024-11-04 11:56:57 +0100

    fix: cast choices target values

--- pathways/typing/tree.py
+++ pathways/typing/tree.py
@@ -235,6 +235,7 @@ class SurveyNode(Node):
                 choices = []
                 for choice in self.parent.choices:
                     # target value should be numeric
+                    print(choice)
                     target_value = choice["target_value"]
                     if isinstance(target_value, str):
                         target_value = float(target_value)
