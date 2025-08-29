1ac275b27b04e0f1272c69598561ed936e00eb39
Yann Forget <yannforget@mailbox.org>
2025-08-29 12:40:22 +0200

    feat: compatibility with new screening config format

--- pathways/typing/screening.py
+++ pathways/typing/screening.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def _required(question_type: str) -> str | None:
     if question_type in ["select_one", "text", "integer", "decimal"]:
         return "TRUE"
@@ -7,36 +10,41 @@ def _required(question_type: str) -> str | None:
         return None
 
 
+def merge_rows(
+    rows_a: list[dict[str, str | None]], rows_b: list[dict[str, str | None]]
+) -> list[dict[str, str | None]]:
+    """Merge two lists of rows with the union of their keys."""
+    keys: set[str] = set()
+    for row in rows_a + rows_b:
+        keys.update(row.keys())
+
+    merge: list[dict[str, str | None]] = []
+    for src_row in rows_a + rows_b:
+        merged_row = {key: src_row.get(key) for key in keys}
+        merge.append(merged_row)
+
+    return merge
+
+
 def add_screening_questions(
-    survey_worksheet: list[dict], screening_config: list[dict], settings_config: dict
+    survey_worksheet: list[dict], screening_questions: list[dict], settings_config: dict
 ) -> list[dict]:
     """Add screening questions to the survey worksheet.
 
     Args:
         survey_worksheet (list[dict]): survey worksheet
-        screening_config (list[dict]): screening configuration data with one dict per screening
+        screening_questions (list[dict]): screening configuration data with one dict per screening
             question
         settings_config (dict): form-level settings
     Returns:
         list[dict]: updated survey worksheet
     """
-    begin = []
-    end = []
+    screening_rows: list[dict[str, Any]] = []
 
-    for question in screening_config:
-        required = _required(question["type"])
-        question_type = question["type"]
-
-        if question_type in ["select_one", "select_multiple"]:
-            question_type = f"{question_type} {question['choice_list']}"
-
-        row = {
-            "type": question_type,
-            "name": question["name"],
-            "calculation": question.get("calculation"),
-            "relevant": question.get("relevant"),
-            "required": required,
-        }
+    for question in screening_questions:
+        row = question.copy()
+        if not row.get("required"):
+            row["required"] = _required(question["type"])
 
         # multi-language "required" messages
         # column for required_message can be either:
@@ -44,53 +52,27 @@ def add_screening_questions(
         #   - required_message::English (en)
         #   - required_message::French (fr)
         #   - etc
-        if required == "TRUE":
+        if row["required"] == "TRUE":
             for key, value in settings_config.items():
                 if key.startswith("required_message"):
                     row[key] = value
 
-        for label_column in [key for key in question.keys() if key.startswith("label")]:
-            row[label_column] = question[label_column]
+        screening_rows.append(row)
 
-        for hint_column in [key for key in question.keys() if key.startswith("hint")]:
-            row[hint_column] = question[hint_column]
-
-        if question["where"] == "begin":
-            begin.append(row)
-        else:
-            end.append(row)
-
-    new_worksheet = begin + survey_worksheet + end
-    return new_worksheet
+    return merge_rows(survey_worksheet, screening_rows)
 
 
 def add_screening_choices(
-    choices_worksheet: list[dict], screening_config: list[dict], choices_config: dict
+    choices_worksheet: list[dict],
+    screening_choices: list[dict],
 ) -> list[dict]:
     """Add screening choices to the choices worksheet.
 
     Args:
         choices_worksheet (list[dict]): choices worksheet
-        screening_config (list[dict]): screening configuration data with one dict per screening
-            question
-        choices_config (dict): choices configuration data with choice list name as key
+        screening_choices (list[dict]): screening configuration data with one dict per choice
+
     Returns:
         list[dict]: updated choices worksheet
     """
-    for question in screening_config:
-        choice_list = question.get("choice_list")
-
-        if choice_list:
-            for choice in choices_config[choice_list]:
-                row = {
-                    "list_name": choice_list,
-                    "value": choice["name"],
-                }
-
-                for label_column, label in choice["label"].items():
-                    row[label_column] = label
-
-                if row not in choices_worksheet:
-                    choices_worksheet.append(row)
-
-    return choices_worksheet
+    return merge_rows(screening_choices, choices_worksheet)
